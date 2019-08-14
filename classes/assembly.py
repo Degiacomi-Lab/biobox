1b002f9676b15cf067aa9d4f97d67fa81743f1fe
Matteo Degiacomi <matteo.degiacomi@gmail.com>
2019-08-14 15:39:07 +0100

    in assembly, integration of method for fiber symmetries

--- classes/assembly.py
+++ classes/assembly.py
@@ -494,3 +494,319 @@ class Assembly(object):
                 fout.write(L)
 
         fout.close()
+
+
+    @ staticmethod
+    def _components(fibertype):
+        if fibertype == 'pmm':
+            return ['p2', 'pm']
+
+        # TODO
+        elif fibertype == 'cmm':
+
+            return ['p2', 'cm']
+        # TODO
+        elif fibertype == 'pmg':
+
+            return
+        
+        # TODO
+        elif fibertype == 'pgg':
+
+            return
+
+        # TODO
+        elif fibertype == 'p31m':
+
+            return
+
+        # TODO
+        elif fibertype == 'p3m1':
+
+            return
+
+        # TODO
+        elif fibertype == 'p4g':
+
+            return
+
+        # TODO
+        elif fibertype == 'p4m':
+
+            return
+
+        # TODO
+        elif fibertype == 'p6m':
+
+            return
+        
+        else:
+            return [fibertype]
+
+    @ staticmethod
+    def num_units_fiber(Lpx, Lpy, min_height=10, fibertype=None):
+        '''
+        calculate number of repeting units to be used to form a fiber.
+
+        :param Lpx: distance of the partner (point that will be superimposed to the origin) along x as number of steps in a 2D tiling.
+        :param Lpy: distance of the partner (point that will be superimposed to the origin) along y as number of steps in a 2D tiling.
+        :param min_height: optional, minimal height (number of repeting units along y) of the fiber (if min_height < Lpy, Lpy will be used as height of the fiber). Default is 10.
+        '''
+
+        Nx = Lpx
+        Ny = max(min_height, Lpy)
+
+        if fibertype:
+            components = Assembly._components(fibertype)
+            n = len(components)
+        else:
+            n = 1
+
+        return n * Nx, Ny
+
+    def make_fiber(self, vx, Lpx, Lpy, vy=None, gamma=np.pi/2, v=0, min_height=2, fibertype='p1oblique'):
+        '''
+        create a fiber, seen as the rolling of a plane with (vx, vy) tiling such that the repeating unit in position (Lpx, Lpy) will be overlapped to the origin.
+
+        :param vx: distance between two first neighbors along x in a 2D tiling.
+        :param Lpx: distance of the partner (point that will be superimposed to the origin) along x as number of steps in a 2D tiling.
+        :param Lpy: distance of the partner (point that will be superimposed to the origin) along y as number of steps in a 2D tiling.
+        :param vy: optional, distance between two first neighbors along y in a 2D tiling (needed for 'p1oblique', 'p1rectangular', 'pm', 'pg', 'p2', ... fiber types).
+        :param gamma: optional, angle between vx and vy in rad, needed for 'p1oblique' fiber type and ignored for other fiber types (default is pi/2 - equivalent to p1rectangular).
+        :param min_height: optional, minimal height (number of repeting units along y) of the fiber (if min_height < Lpy, Lpy will be used as height of the fiber). Default is 2.
+        :param fibertype: optional, can be 'p1rectangular', 'p1hexagonal', 'p1oblique', pm', 'pg', 'cm', ... (default is 'p1oblique').
+        :param v: optional, additional parameter needed for 'pm', 'pg', 'cm', 'p2', ... fiber types (default is None). List for composite fibertypes.
+        '''
+
+        if type(v) == int or type(v) == float:
+            vlist = [v]
+        else:
+            vlist = list(v)
+
+        if fibertype not in ['p1rectangular', 'p1oblique', 'p1hexagonal', 'pm', 'pg', 'cm', 'p2', 'p3', 'p4', 'p6', 'pmm', 'cmm']:
+            raise Exception("fibertype %s not valid." %(fibertype))
+
+        def lvalue(Lx, Ly, vx, vy):
+            return np.sqrt((Lx * vx) ** 2 + (Ly * vy) ** 2)
+
+        def thetavalue(Lx, Ly, vx, vy):
+            try:
+                return np.arctan2(Ly * vy , Lx * vx)
+            except ZeroDivisionError:
+                return np.sign(Ly) * np.pi / 2
+
+        def phivalue(L, theta, thetap, Lp):
+            return (2 * np.pi * L * np.cos(theta - thetap) / Lp) + np.pi
+            # return (2 * np.pi * L * np.cos(theta - thetap) / Lp)
+
+
+        def coords_in_fiber(Lx, Ly, vx, vy, Lp, x0, y0, z0, thetap):
+            L = lvalue(Lx, Ly, vx, vy)
+            theta = thetavalue(Lx, Ly, vx, vy)
+            phi = phivalue(L, theta, thetap, Lp)
+            Lp2pi = float(Lp) / (2 * np.pi)
+            x2 = (Lp2pi + x0) * np.cos(phi) + z0 * np.sin(phi)
+            z = - (Lp2pi + x0) * np.sin(phi) + z0 * np.cos(phi)
+            x = x2 + Lp2pi
+            y = L * np.sin(theta - thetap) + y0
+            
+            return x, y, z
+            
+
+        transformations = self._components(fibertype)
+
+        if len(vlist) != len(transformations):
+            raise Exception("%s parameters needed for %s fiber but only %s passed." %(len(transformations), fibertype, len(vlist)))
+
+        psi = None
+
+        nunitsdict ={'p1oblique': 1,
+        'p1rectangular': 1,
+        'p1hexagonal': 1,
+        'p2': 2,
+        'p3': 3,
+        'p4': 2,
+        'p6': 6,
+        'pm': 2,
+        'pg': 1,
+        'cm': 2} # or cm 1?
+
+        nunits = [nunitsdict[t] for t in transformations]
+        nunits_tot = sum(nunits)
+        t_order = [[(t, v)] * n for (t, v, n) in zip(transformations, vlist, nunits)]
+        t_order = [v for sublist in t_order for v in sublist]
+
+
+        if Lpx % nunits_tot != 0:
+            raise Exception(
+                    "ERROR: Lpx must be a multiple of %s when doing a %s tiling!" %(nunits_tot, fibertype))
+
+
+        if 'p1hexagonal' in transformations or 'p2' in transformations or 'p3' in transformations or 'p4' in transformations or 'p6' in transformations or 'pg' in transformations or 'pm' in transformations:
+            if Lpy % 2 == 1:
+                raise Exception(
+                    "ERROR: Lpy must be even when doing a %s tiling!" %(fibertype))
+            vy = np.sqrt(3) * vx / 2
+            if 'p6' in transformations:
+                vy = 3 * vy
+
+        if 'p1oblique' in transformations:
+            vy = vy * np.sin(gamma)
+        
+        if 'p2' in transformations:
+            psi = np.arcsin(float(vy) / np.sqrt(vx ** 2 + vy ** 2))
+
+        if 'p4' in transformations or 'cm' in transformations:
+            vy = vx
+
+        Lp = lvalue(Lpx, Lpy, vx, vy)
+        thetap = thetavalue(Lpx, Lpy, vx, vy)
+        Nx, _ = Assembly.num_units_fiber(Lpx, Lpy, min_height=min_height)
+
+        def basic_transform(u, Lx, Ly, fibertype, vx, vy, gamma, v, psi):
+            if fibertype == 'p1hexagonal' and Ly % 2 == 1:
+                Lx += .5
+            
+            elif fibertype == 'p1oblique' and Ly % 2 == 1:
+                Lx += vy * np.cos(gamma)
+
+            elif fibertype == 'p2':
+                odd = (Ly % 2 == 1)
+                if Lx % 2 == 1:
+                    u.rotate(180, 0, 0) # why around x on not z??
+                    Lx += v * np.cos(psi) / vx
+                    Ly -= v * np.sin(psi) / vy
+                    Lx -= 1
+                else:
+                    Lx -= v * np.cos(psi) / vx
+                    Ly += v * np.sin(psi) / vy
+                if odd:
+                    Lx += 1
+
+            elif fibertype == 'p3':
+                odd = (Ly % 2 == 1)
+                if Lx % 3 == 1:
+                    u.rotate(-120, 0, 0) # why around x on not z??
+                    Lx -= v * np.cos(np.pi / 6) / vx
+                    Ly -= v * np.sin(np.pi / 6) / vy
+                    Lx -= 1
+                elif Lx % 3 == 2:
+                    u.rotate(120, 0, 0) # why around x on not z??
+                    Lx += v * np.cos(np.pi / 6) / vx
+                    Ly -= v * np.sin(np.pi / 6) / vy
+                    Lx -= 2
+                else:
+                    Ly += float(v) / vy
+                if odd:
+                    Lx += 1.5
+
+            elif fibertype == 'p4':
+                if Lx % 2 == 1:
+                    if Ly % 2 == 1:
+                        u.rotate(-90, 0, 0)
+                        Lx -= v * np.cos(np.pi / 4) / vx
+                    else:
+                        u.rotate(180, 0, 0)
+                        Lx += v * np.cos(np.pi / 4) / vx
+                        Lx -= 1
+                                       
+                else:
+                    if Ly % 2 == 1:
+                        u.rotate(90, 0, 0)
+                        Lx += v * np.cos(np.pi / 4) / vx
+                        Lx += 1
+                    else:
+                        Lx -= v * np.cos(np.pi / 4) / vx
+                    Ly -= v * np.sin(np.pi / 4) / vy 
+            
+            elif fibertype == 'p6':
+                odd = (Ly % 2 == 1)
+                if Lx % 6 == 1:
+                    u.rotate(-60, 0, 0) # why around x on not z??
+                    Lx += v * np.cos(np.pi / 6) / vx
+                    Ly += v * np.sin(np.pi / 6) / vy
+                    Lx -= 1
+                elif Lx % 6 == 2:
+                    u.rotate(-120, 0, 0) # why around x on not z??
+                    Lx += v * np.cos(np.pi / 6) / vx
+                    Ly -= v * np.sin(np.pi / 6) / vy
+                    Lx -= 2
+                elif Lx % 6 == 3:
+                    u.rotate(-180, 0, 0) # why around x on not z??
+                    Ly -= float(v) / vy
+                    Lx -= 3
+                elif Lx % 6 == 4:
+                    u.rotate(-240, 0, 0) # why around x on not z??
+                    Lx -= v * np.cos(np.pi / 6) / vx
+                    Ly -= v * np.sin(np.pi / 6) / vy
+                    Lx -= 4
+                elif Lx % 6 == 5:
+                    u.rotate(-300, 0, 0) # why around x on not z??
+                    Lx -= v * np.cos(np.pi / 6) / vx
+                    Ly += v * np.sin(np.pi / 6) / vy
+                    Lx -= 5
+                else:
+                    Ly += float(v) / vy
+                if odd:
+                    Lx += 3
+
+            elif  fibertype == 'pm':
+                if Lx % 2 == 0:
+                    Lx += float(v) / vx
+                else:
+                    u.rotate(0, 180, 0)
+                    Lx -= 1 + float(v) / vx
+
+            elif fibertype == 'pg':
+                if Ly % 2 == 0:
+                    Lx += float(v) / vx
+                else:
+                    u.rotate(0, 180, 0)
+                    Lx -= float(v) / vx
+
+            elif fibertype == 'cm':
+                if Lx % 2 == 0:
+                    Lx += float(v) / vx
+                else:
+                    u.rotate(0, 180, 0)
+                    Lx -= 1 + float(v) / vx
+                if Ly % 2 == 1:
+                    Lx += 1
+
+            return Lx, Ly
+
+        # print(zip(transformations, vlist))
+
+        composite = (len(transformations) > 1)
+        if not composite:
+            t = transformations[0]
+            v = vlist[0]
+            for n, u in enumerate(self.unit):
+                Ly = n / Nx
+                Lx = n % Nx
+                Lx, Ly = basic_transform(u, Lx, Ly, t, vx, vy, gamma, v, psi)
+                coords_fiber = np.array([coords_in_fiber(Lx, Ly, vx, vy, Lp, x0, y0, z0, thetap) for [x0, y0, z0] in u.get_xyz()])
+                self.unit[n].set_xyz(coords_fiber)
+
+        else:
+            for n, u in enumerate(self.unit):
+                Ly = n / Nx
+                Lx = n % Nx
+
+                Lx1 = Lx
+                Ly1 = Ly
+                for i, (t, v) in enumerate(zip(transformations, vlist)):
+                    nu = nunitsdict[t]
+                    if n >= nu * i:
+                        Lxt, Lyt = basic_transform(u, Lx, Ly, t, vx, vy, gamma, v, psi)
+                        Lx1 += Lxt
+                        Lyt += Lyt
+
+                Lx = Lx1
+                Ly = Ly1
+                coords_fiber = np.array([coords_in_fiber(Lx, Ly, vx, vy, Lp, x0, y0, z0, thetap) for [x0, y0, z0] in u.get_xyz()])
+                self.unit[n].set_xyz(coords_fiber)
+
+
+
+        
